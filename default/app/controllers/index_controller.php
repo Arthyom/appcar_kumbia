8fb067d495503d235462b33ed8e31a03b837f0ab
arthyom <a.gonzalezgavina@ugto.mx>
2023-03-08 02:12:59 -0600

    Removing unnecesary code

--- default/app/controllers/index_controller.php
+++ default/app/controllers/index_controller.php
@@ -8,56 +8,6 @@ class IndexController extends AppController
 {
     public function index()
     {
-        /*
-        try {
-            View::select(null);
-            View::template(null);
-
-            $pull = "/usr/bin/git pull https://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@github.com/arthyom/appcar_kumbia.git  master 2>&1";
-            $response = shell_exec($pull);
-            echo var_dump($response);
-
-            echo('okokoko ???');
-        } catch ( Throwable $th) {
-            echo('error');
-            echo var_dump($th);
-            //throw $th;
-        }
-       */
-
         echo 'hola desde el index';
-    }
-
-    public function hola()
-    {
-        # code...
-        View::select(null);
-        View::template(null);
-        echo 'holaaa';
-    }
-
-    public function adios()
-    {
-        # code...
-        View::select(null);
-        View::template(null);
-        echo 'adios';
-    }
-
-    public function saludos()
-    {
-        # code...
-        View::select(null);
-        View::template(null);
-        echo 'saludos';
-    }
-
-    public function regards()
-    {
-        # code...
-        View::select(null);
-        View::template(null);
-        echo 'saludos';
-        echo 'and best regards';
     }
 }
